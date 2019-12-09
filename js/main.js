1d1612e4f38586b9965b4f0fcc645e9f444967c3
gstark0 <gstark0@icloud.com>
2019-12-09 15:56:36 +0100

    Headline animation

--- js/main.js
+++ js/main.js
@@ -36,3 +36,36 @@ function scrollIt(destination, duration=1000, callback) {
 
     scroll();
 }
+
+
+// Headline animation
+var animationDelay = 2500;
+animateHeadline(document.getElementById('main-header'));
+
+function animateHeadline(headline) {
+    setTimeout(function(){
+        hideWord( headline.getElementsByClassName('is-visible')[0] )
+    }, animationDelay);
+}
+
+function hideWord(word) {
+    var nextWord = takeNext(word);
+    switchWord(word, nextWord);
+    setTimeout(function(){
+        hideWord(nextWord)
+    }, animationDelay);
+}
+
+function takeNext(word) {
+    let nextWord = word.nextElementSibling;
+    if(nextWord != null)
+        return nextWord;
+    else {
+        return word.parentElement.firstElementChild;
+    }
+}
+
+function switchWord(oldWord, newWord) {
+    oldWord.className = 'is-hidden';
+    newWord.className = 'is-visible';
+}
